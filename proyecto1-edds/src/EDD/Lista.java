f7bc596dd0034dc4edbafebcd279e895ffe7ce37
JorgeMartinez05 <jorge2005.martinez@gmail.com>
2024-10-17 13:29:58 -0400

    Update Lista.java

--- proyecto1-edds/src/EDD/Lista.java
+++ proyecto1-edds/src/EDD/Lista.java
@@ -9,5 +9,44 @@ package EDD;
  * @author jorge
  */
 public class Lista {
+    private Nodo pFirst;
+    private int size; 
+    
+   
+    public Lista() {
+        this.pFirst = null;
+        this.size = 0; 
+    }
+    
+    // get y set para los atrubutos
+
+    public Nodo getpFirst() {
+        return pFirst;
+    }
+
+    public void setpFirst(Nodo pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    //Primitivas
+    
+    // Verficar si la lista es vacia
+    public boolean isEmpty(){
+        return this.pFirst == null;
+    }
+    
+    //Destructor
+    public void eliminar(){
+        pFirst = null;
+        size = 0;
+    }
     
 }
